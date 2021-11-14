e9051f222305210a2399cf68487efeaafb76fbf7
“leonshimizu” <lmshimizu@gmail.com>
2021-11-14 10:28:14 -0800

    added link on top of page

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@
       <router-link to="/signup">Sign Up</router-link> |
       <router-link to="/login">Login</router-link> |
       <router-link to="/logout">Logout</router-link> |
-      <router-link to="/blogs">Blogs</router-link>
+      <router-link to="/blogs">Posts</router-link> |
+      <router-link to="/posts/new">New Post</router-link>
     </div>
     <router-view/>
   </div>
