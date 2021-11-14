fb36878424f95163d4c6aad83166b2f39d481368
“leonshimizu” <lmshimizu@gmail.com>
2021-11-14 08:24:18 -0800

    added test page

---
+++ src/views/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="test">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Test!",
+      };
+    },
+    created: function () {},
+    methods: {},
+  };
+</script>
